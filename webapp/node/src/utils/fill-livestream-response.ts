405e59a216fae0f877051d8e27c8b178b4654301
Takahiro Nishino <bigstitch99@gmail.com>
2023-11-25 13:35:29 +0900

    Update fill-livestream-response.ts

--- webapp/node/src/utils/fill-livestream-response.ts
+++ webapp/node/src/utils/fill-livestream-response.ts
@@ -1,50 +1,49 @@
-import { PoolConnection, RowDataPacket } from 'mysql2/promise'
+import { PoolConnection, RowDataPacket } from "mysql2/promise";
 import {
   LivestreamTagsModel,
   LivestreamsModel,
   TagsModel,
   UserModel,
-} from '../types/models'
-import { UserResponse, fillUserResponse } from './fill-user-response'
+} from "../types/models";
+import { UserResponse, fillUserResponse } from "./fill-user-response";
 
 export interface LivestreamResponse {
-  id: number
-  owner: UserResponse
-  title: string
-  tags: { id: number; name: string }[]
-  description: string
-  playlist_url: string
-  thumbnail_url: string
-  start_at: number
-  end_at: number
+  id: number;
+  owner: UserResponse;
+  title: string;
+  tags: { id: number; name: string }[];
+  description: string;
+  playlist_url: string;
+  thumbnail_url: string;
+  start_at: number;
+  end_at: number;
 }
 
 export const fillLivestreamResponse = async (
   conn: PoolConnection,
   livestream: LivestreamsModel,
-  getFallbackUserIcon: () => Promise<Readonly<ArrayBuffer>>,
+  getFallbackUserIcon: () => Promise<Readonly<ArrayBuffer>>
 ) => {
   const [[user]] = await conn.query<(UserModel & RowDataPacket)[]>(
-    'SELECT * FROM users WHERE id = ?',
-    [livestream.user_id],
-  )
-  if (!user) throw new Error('not found user that has the given id')
+    "SELECT * FROM users WHERE id = ?",
+    [livestream.user_id]
+  );
+  if (!user) throw new Error("not found user that has the given id");
 
-  const userResponse = await fillUserResponse(conn, user, getFallbackUserIcon)
+  const userResponse = await fillUserResponse(conn, user, getFallbackUserIcon);
 
-  const [livestreamTags] = await conn.query<
-    (LivestreamTagsModel & RowDataPacket)[]
-  >('SELECT * FROM livestream_tags WHERE livestream_id = ?', [livestream.id])
+  const [livestreamTagIDs] = await conn.query<
+    ({ tag_id: number } & RowDataPacket)[]
+  >("SELECT tag_id FROM livestream_tags WHERE livestream_id = ?", [
+    livestream.id,
+  ]);
 
-  const tags: TagsModel[] = []
-  for (const livestreamTag of livestreamTags) {
-    const [[tag]] = await conn.query<(TagsModel & RowDataPacket)[]>(
-      'SELECT * FROM tags WHERE id = ?',
-      [livestreamTag.tag_id],
-    )
-    tags.push(tag)
-  }
-// add owner and tag
+  // const tags: TagsModel[] = [];
+  const [tags] = await conn.query<(TagsModel & RowDataPacket)[]>(
+    "SELECT * FROM tags WHERE id IN (?)",
+    [livestreamTagIDs.map(({ tag_id }) => tag_id)]
+  );
+  // add owner and tag
   return {
     id: livestream.id,
     owner: userResponse,
@@ -55,5 +54,5 @@ export const fillLivestreamResponse = async (
     thumbnail_url: livestream.thumbnail_url,
     start_at: livestream.start_at,
     end_at: livestream.end_at,
-  } satisfies LivestreamResponse
-}
+  } satisfies LivestreamResponse;
+};
