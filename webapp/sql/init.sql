85b6fc0fcc2cf5e6be466af5f9b13583c0289cbf
Takahiro Nishino <bigstitch99@gmail.com>
2023-11-25 15:03:28 +0900

    Update init.sql

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -22,13 +22,13 @@ ALTER TABLE `tags` auto_increment = 1;
 ALTER TABLE `livecomments` auto_increment = 1;
 ALTER TABLE `livestreams` auto_increment = 1;
 ALTER TABLE `users` auto_increment = 1;
-IF NOT EXISTS(
-    SELECT NULL
-    FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE table_name = 'users'
-        AND table_schema = 'isupipe'
-        AND column_name = 'dark_mode'
-) THEN
-ALTER TABLE `users`
-ADD `dark_mode` BOOLEAN NOT NULL;
-END IF;
+-- IF NOT EXISTS(
+--     SELECT NULL
+--     FROM INFORMATION_SCHEMA.COLUMNS
+--     WHERE table_name = 'users'
+--         AND table_schema = 'isupipe'
+--         AND column_name = 'dark_mode'
+-- ) THEN
+-- ALTER TABLE `users`
+-- ADD `dark_mode` BOOLEAN NOT NULL
+-- END IF;
