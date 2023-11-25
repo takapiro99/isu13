06b2ce1d10a322033f38e60eaaf1532398a8c019
Takahiro Nishino <bigstitch99@gmail.com>
2023-11-25 13:50:36 +0900

    Update top-handler.ts

--- webapp/node/src/handlers/top-handler.ts
+++ webapp/node/src/handlers/top-handler.ts
@@ -1,86 +1,79 @@
-import { Context } from 'hono'
-import { RowDataPacket } from 'mysql2/promise'
-import { HonoEnvironment } from '../types/application'
-import { verifyUserSessionMiddleware } from '../middlewares/verify-user-session-middleare'
-import { TagsModel, ThemeModel, UserModel } from '../types/models'
-import { throwErrorWith } from '../utils/throw-error-with'
+import { Context } from "hono";
+import { RowDataPacket } from "mysql2/promise";
+import { HonoEnvironment } from "../types/application";
+import { verifyUserSessionMiddleware } from "../middlewares/verify-user-session-middleare";
+import { TagsModel, ThemeModel, UserModel } from "../types/models";
+import { throwErrorWith } from "../utils/throw-error-with";
 
 // GET /api/tag
 export const getTagHandler = async (
-  c: Context<HonoEnvironment, '/api/tag'>,
+  c: Context<HonoEnvironment, "/api/tag">
 ) => {
-  const conn = await c.get('pool').getConnection()
-  await conn.beginTransaction()
+  const conn = await c.get("pool").getConnection();
+  await conn.beginTransaction();
 
   try {
     const [tags] = await conn
-      .execute<(TagsModel & RowDataPacket)[]>('SELECT * FROM tags')
-      .catch(throwErrorWith('failed to get tags'))
+      .execute<(TagsModel & RowDataPacket)[]>("SELECT * FROM tags")
+      .catch(throwErrorWith("failed to get tags"));
 
-    await conn.commit().catch(throwErrorWith('failed to commit'))
+    await conn.commit().catch(throwErrorWith("failed to commit"));
 
-    const tagResponses = []
+    const tagResponses = [];
     for (const tag of tags) {
       tagResponses.push({
         id: tag.id,
         name: tag.name,
-      })
+      });
     }
 
-    return c.json({ tags: tagResponses })
+    return c.json({ tags: tagResponses });
   } catch (error) {
-    await conn.rollback()
-    return c.text(`Internal Server Error\n${error}`, 500)
+    await conn.rollback();
+    return c.text(`Internal Server Error\n${error}`, 500);
   } finally {
-    await conn.rollback()
-    conn.release()
+    await conn.rollback();
+    conn.release();
   }
-}
+};
 
 // 配信者のテーマ取得API
 // GET /api/user/:username/theme
 export const getStreamerThemeHandler = [
   verifyUserSessionMiddleware,
-  async (c: Context<HonoEnvironment, '/api/user/:username/theme'>) => {
-    const username = c.req.param('username')
+  async (c: Context<HonoEnvironment, "/api/user/:username/theme">) => {
+    const username = c.req.param("username");
 
-    const conn = await c.get('pool').getConnection()
-    await conn.beginTransaction()
+    const conn = await c.get("pool").getConnection();
+    await conn.beginTransaction();
 
     try {
       const [[user]] = await conn
-        .execute<(Pick<UserModel, 'id'> & RowDataPacket)[]>(
-          'SELECT id FROM users WHERE name = ?',
-          [username],
+        .execute<(Pick<UserModel, "id" | "dark_mode"> & RowDataPacket)[]>(
+          "SELECT id, dark_mode FROM users WHERE name = ?",
+          [username]
         )
-        .catch(throwErrorWith('failed to get user'))
+        .catch(throwErrorWith("failed to get user"));
 
       if (!user) {
-        await conn.rollback()
-        return c.text('not found user that has the given username', 404)
+        await conn.rollback();
+        return c.text("not found user that has the given username", 404);
       }
 
-      const [[theme]] = await conn
-        .execute<(ThemeModel & RowDataPacket)[]>(
-          'SELECT * FROM themes WHERE user_id = ?',
-          [user.id],
-        )
-        .catch(throwErrorWith('failed to get user theme'))
-
-      await conn.commit().catch(throwErrorWith('failed to commit'))
+      await conn.commit().catch(throwErrorWith("failed to commit"));
 
       const themeResponse = {
-        id: theme.id,
-        dark_mode: !!theme.dark_mode,
-      }
+        id: user.id,
+        dark_mode: user.dark_mode,
+      };
 
-      return c.json(themeResponse)
+      return c.json(themeResponse);
     } catch (error) {
-      await conn.rollback()
-      return c.text(`Internal Server Error\n${error}`, 500)
+      await conn.rollback();
+      return c.text(`Internal Server Error\n${error}`, 500);
     } finally {
-      await conn.rollback()
-      conn.release()
+      await conn.rollback();
+      conn.release();
     }
   },
-]
+];
